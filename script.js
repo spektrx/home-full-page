7bdac9d7e4f6ac85b2b25a6720f6e340c1cb86a6
spektrx <silverdenis05@gmail.com>
2025-01-29 20:09:54 +0300

    2

--- script.js
+++ script.js
@@ -21,11 +21,11 @@ for (let i = 0; i < acc.length; i++) {
     this.classList.toggle("active");
 
     /* Toggle between hiding and showing the active panel */
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
+    let panel = this.nextElementSibling;
+    if (panel.style.display === "flex") {
       panel.style.display = "none";
     } else {
-      panel.style.display = "block";
+      panel.style.display = "flex";
     }
   });
 }
